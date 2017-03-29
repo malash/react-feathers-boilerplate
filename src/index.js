7b25ebee27b1f2abe111958da2c5a2b2e694766b
Malash <i@malash.me>
2017-03-29 11:54:03 +0800

    fix production mode fallback

--- src/index.js
+++ src/index.js
@@ -12,14 +12,23 @@ const compress = require('compression');
 const cors = require('cors');
 
 const app = feathers();
-app.configure(configuration(path.join(__dirname, '..')))
+app
+  .configure(configuration(path.join(__dirname, '..')))
   .use(compress())
   .options('*', cors())
   .use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(favicon( path.join(app.get('public'), 'favicon.ico') ))
-    .use('/', serveStatic( app.get('public') ));
+  app
+    .use(favicon( path.join(app.get('public'), 'favicon.ico') ))
+    .use('/', serveStatic( app.get('public') ))
+    .use((req, res, next) => {
+      if (!req.url.startsWith('/api')) {
+        res.sendFile(path.join(app.get('public'), 'index.html'));
+      } else {
+        return next(null);
+      }
+    });
 }
 
 app.use('/api/v1', require('./app'));
